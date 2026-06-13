d193ca677324eb0fa46d03e3aafe36888cfa0cba
am1right <am1rightwork@gmail.com>
2026-06-13 03:48:20 +0300

    1

--- webapp/js/navbar.js
+++ webapp/js/navbar.js
@@ -8,6 +8,7 @@
         if (name === 'games') return /\/games\//.test(path) || /index\.html$/.test(path) || /\/$/.test(path);
         if (name === 'profile') return /profile\.html$/.test(path);
         if (name === 'shop') return /shop\.html$/.test(path);
+        if (name === 'cases') return /cases\.html$/.test(path);
         return false;
     }
 
@@ -28,6 +29,10 @@
         <a class="${cls('shop')}" href="${prefix}shop.html">
             <span class="chin-navbar__icon"><img src="${prefix}icons/shop.png" alt="Магазин" style="object-fit:contain;filter:drop-shadow(0 0 5px rgba(255,255,255,0.3));"></span>
             <span>Магазин</span>
+        </a>
+        <a class="${cls('cases')}" href="${prefix}cases.html">
+            <span class="chin-navbar__icon"><img src="${prefix}icons/box.webp" alt="Кейсы" style="object-fit:contain;filter:drop-shadow(0 0 5px rgba(255,255,255,0.3));"></span>
+            <span>Кейсы</span>
         </a>
     </nav>`;
 
